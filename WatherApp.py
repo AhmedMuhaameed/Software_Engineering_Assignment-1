9c5b8145c5343405ac2971d2b6fa14b15855dc94
AhmedEhabH <ahmed.ehab.h@gmail.com>
2019-02-23 15:54:26 +0200

    Location Detect Done

--- WatherApp.py
+++ WatherApp.py
@@ -10,9 +10,10 @@ if r.status_code >= 200 < 400:
     print(data)
     result = json.loads(data)
     print(result)
+    location = result['city'] + ',' + result['countryCode']
 #=======================
 # Weather API
-api_key = '9bb4b43bf13147ef8628e69c31b866ad'
+    api_key = '9bb4b43bf13147ef8628e69c31b866ad'
     location = 'Cairo,EG'
     url = 'https://api.weatherbit.io/v2.0/current?city=' + location + '&key=' + api_key
     r = requests.get(url)
