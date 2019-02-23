a978fbab161dfdf357d2d5a33a4b8e1bf6deb643
AhmedEhabH <ahmed.ehab.h@gmail.com>
2019-02-23 15:34:00 +0200

    IP API

--- WatherApp.py
+++ WatherApp.py
@@ -1,1 +1,12 @@
 #Weather API
+import requests
+import json
+
+url = 'http://ip-api.com/json'
+
+r = requests.get(url)
+if r.status_code >= 200 < 400:
+    data = r.text
+    print(data)
+    result = json.loads(data)
+    print(result)
