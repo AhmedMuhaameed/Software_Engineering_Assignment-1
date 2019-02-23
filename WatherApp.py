66fdb0f46eaff8c087bb5b0664bfeb3db2070dfe
ahmedmagdy512 <45365052+ahmedmagdy512@users.noreply.github.com>
2019-02-23 15:39:46 +0200

    Update WatherApp.py

--- WatherApp.py
+++ WatherApp.py
@@ -10,3 +10,16 @@ if r.status_code >= 200 < 400:
     print(data)
     result = json.loads(data)
     print(result)
+#=======================
+# Weather API
+api_key = '9bb4b43bf13147ef8628e69c31b866ad'
+    location = 'Cairo,EG'
+    url = 'https://api.weatherbit.io/v2.0/current?city=' + location + '&key=' + api_key
+    r = requests.get(url)
+    status = r.status_code
+    if status >= 200 < 400:
+        data = r.text
+        result = json.loads(data)
+        print(data)
+        print(result)
+#===================== End of Weather api
