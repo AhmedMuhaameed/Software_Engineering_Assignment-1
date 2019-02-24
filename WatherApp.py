33510905b0c9ec3b93f6e19dd456606a8e2b5db6
AhmedEhabH <ahmed.ehab.h@gmail.com>
2019-02-24 21:39:59 +0200

    Changed done and i get loccation

--- WatherApp.py
+++ WatherApp.py
@@ -1,26 +1,26 @@
-#Weather API
-import requests
-import json
+ # Weather API
+import requests # pip install requests
+import json # pip install simplejson
 
+user_location = 'London,UK'
+
+# IP API
 url = 'http://ip-api.com/json'
+requestLocation = requests.get(url)
+if requestLocation.status_code >= 200 < 400:
+	data = json.loads(requestLocation.text)
+	user_location = data['city'] + ',' + data['countryCode']
 
-r = requests.get(url)
-if r.status_code >= 200 < 400:
-    data = r.text
-    print(data)
-    result = json.loads(data)
-    print(result)
-    location = result['city'] + ',' + result['countryCode']
-#=======================
+#==============================================================
 # Weather API
-    api_key = '9bb4b43bf13147ef8628e69c31b866ad'
-    location = 'Cairo,EG'
-    url = 'https://api.weatherbit.io/v2.0/current?city=' + location + '&key=' + api_key
-    r = requests.get(url)
-    status = r.status_code
-    if status >= 200 < 400:
-        data = r.text
-        result = json.loads(data)
-        print(data)
-        print(result)
-#===================== End of Weather api
+api_key = '9bb4b43bf13147ef8628e69c31b866ad'
+location = 'Cairo,EG'
+url = 'https://api.weatherbit.io/v2.0/current?city=' + location + '&key=' + api_key
+r = requests.get(url)
+status = r.status_code
+if status >= 200 < 400:
+	data = r.text
+	result = json.loads(data)
+	print(data)
+	print(result)
+#=========================================== End of Weather api
